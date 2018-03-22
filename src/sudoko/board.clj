2430ea6103b8173f436cccbc0518c9e5bc0a9486
Kenneth Hedman <github@vitberget.se>
2018-03-22 20:10:55 +0100

    Created namespace check, moved solved?-stuff there. Created board managment fns in board

--- src/sudoko/board.clj
+++ src/sudoko/board.clj
@@ -1,25 +1,44 @@
 (ns sudoko.board
   (:require [ysera.test :refer [is is-not is=]]))
 
-(comment
-  (def example-board
-    '({:v 1 :x 0 :y 0 :given true})
-    ))
 
-
-(defn solved-part?
+(defn get-row
   {:test (fn []
-           (is (solved-part? [{:v 1} {:v 2} {:v 3} {:v 4} {:v 5} {:v 6} {:v 7} {:v 8} {:v 9}]))
-           (is (solved-part? [{:v 1} {:v 5} {:v 9} {:v 4} {:v 2} {:v 6} {:v 7} {:v 8} {:v 3}])))}
-  [list]
-  (and (= 9 (count list))
-       (->> list
-            (map :v)
-            (into #{})
-            (= #{1 2 3 4 5 6 7 8 9}))))
+           (is= (-> '({:x 0 :y 0} {:x 0 :y 2} {:x 2 :y 0} {:x 2 :y 2} {:x 4 :y 0})
+                    (get-row 0))
+                '({:x 0 :y 0} {:x 2 :y 0} {:x 4 :y 0})))}
+  [board row]
+  (->> board
+       (filter (fn [n]
+                 (when
+                   (= (:y n) row)
+                   n)))))
 
-(defn solved? [board]
-  (and
-    (= (* 9 9) (count board))
+(defn get-column
+  {:test (fn []
+           (is= (-> '({:x 0 :y 0} {:x 0 :y 2} {:x 2 :y 0} {:x 2 :y 2} {:x 4 :y 0})
+                    (get-column 2))
+                '({:x 2 :y 0} {:x 2 :y 2})))}
+  [board column]
+  (->> board
+       (filter (fn [n]
+                 (when
+                   (= (:x n) column)
+                   n)))))
 
-    ))
+(defn get-quadrant
+  {:test (fn []
+           (is= (-> '({:x 0 :y 4} {:x 0 :y 2} {:x 2 :y 0} {:x 2 :y 2} {:x 4 :y 0})
+                    (get-quadrant 0 0))
+                '({:x 0 :y 2} {:x 2 :y 0} {:x 2 :y 2})))}
+  [board row column]
+  (let [row-base (* 3 row)
+        column-base (* 3 column)
+        row-set #{row-base (+ row-base 1) (+ row-base 2)}
+        column-set #{column-base (+ column-base 1) (+ column-base 2)}]
+    (->> board
+          (filter (fn [n]
+                    (when (and (contains? row-set (:x n))
+                               (contains? column-set (:y n)))
+                      n))))))
+
